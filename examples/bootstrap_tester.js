558984714b3d0010fdbe3edd6d3faf329e8321ce
Trent Grover <trent.grover@webfilings.com>
2014-09-17 10:44:32 -0600

    add JSON reports

--- examples/bootstrap_tester.js
+++ examples/bootstrap_tester.js
@@ -22,23 +22,29 @@
         window.jasmineRequire = module;
 
         System.import('./lib/jasmine-2.0.2/console').then(function(module) {
-
             System.import('./lib/jasmine-2.0.2/boot').then(function(module) {
+                System.import('./lib/jasmine-jsreporter/jasmine-jsreporter').then(function(module) {
 
-                // to load an array of specs
-                var specsToRun = [
-                    './lilSpec',
-                    './anotherSpec'
-                ];
-                function importIt(x) {
-                    return System.import(x);
-                }
-                Promise.all(specsToRun.map(importIt)).then(function(){
-                    var env = jasmine.getEnv();
-                    env.execute();
+                    // attach the JSReporter to get JSON test results
+                    jasmine.getEnv().addReporter(new jasmine.JSReporter2());
+
+                    // to load an array of specs
+                    var specsToRun = [
+                        './lilSpec',
+                        './anotherSpec'
+                    ];
+                    function importIt(x) {
+                        return System.import(x);
+                    }
+                    Promise.all(specsToRun.map(importIt)).then(function(){
+                        var env = jasmine.getEnv();
+                        env.execute();
+
+                        // echo out the JSON test results
+                        var testResults = env.getJSReport();
+                        console.log(testResults);
+                    });
                 });
-
             });
-
         });
     });
