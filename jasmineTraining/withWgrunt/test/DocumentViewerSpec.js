34e3f1151c0d949603023f95431d54ca7b35d271
Pat Kujawa <pat.kujawa@webfilings.com>
2014-09-22 15:43:31 -0600

    Formatting.

--- jasmineTraining/withWgrunt/test/DocumentViewerSpec.js
+++ jasmineTraining/withWgrunt/test/DocumentViewerSpec.js
@@ -8,13 +8,22 @@ define(function(require) {
 
         describe('#load', function() {
             it('should call getPageAsync', function () {
+                // ARRANGE
                 var viewer = new DocumentViewer();
+                // var viewer = Object.create(DocumentViewer.prototype);
+                // var viewer = jasmine.createSpyObj('DocumentViewer', ['getPageAsync']);
                 spyOn(viewer, 'getPageAsync');
+
+                // ACT
                 viewer.load();
+
+                // ASSERT
                 expect(viewer.getPageAsync).toHaveBeenCalled();
                 expect(viewer.getPageAsync).toHaveBeenCalledWith(jasmine.any(Function));
+
                 var firstCall = viewer.getPageAsync.calls[0];
                 expect(firstCall.args[0]).toEqual(jasmine.any(Function)); // NOTE: not toBe
+                expect(firstCall.args.length).toEqual(1);
                 expect(typeof firstCall.args[0]).toEqual('function');
             });
 
@@ -26,8 +35,11 @@ define(function(require) {
                     // Instead of making a slow request, finish immediately
                     callback();
                 })
+
                 viewer.load();
+
                 expect(pageReceivedCallback).toHaveBeenCalled();
+                expect(pageReceivedCallback).toHaveBeenCalledWith('page received');
             });
 
             // Jasmine 1.3 with jasmine.async lib to use 'done' callback
@@ -39,6 +51,7 @@ define(function(require) {
                     expect('got here').toBeTruthy();
                     done();
                 });
+
                 viewer.load();
             });
 
@@ -50,7 +63,9 @@ define(function(require) {
                 viewer.onPageReceived(function() {
                     called = true;
                 });
+
                 viewer.load();
+
                 waitsFor(function() {
                     return called;
                 }, 6000, 'Waited 6s for callback to be called but it was not');
@@ -70,8 +85,8 @@ define(function(require) {
                     viewer.onPageReceived(function() {
                         called = true;
                     });
-                    viewer.load();
 
+                    viewer.load();
                     jasmine.Clock.tick(1001);
 
                     expect(called).toBeTruthy('callback to be called');
@@ -92,8 +107,8 @@ define(function(require) {
                     viewer.onPageReceived(function() {
                         called = true;
                     });
-                    viewer.load();
 
+                    viewer.load();
                     jasmine.clock().tick(1001);
 
                     expect(called).toBeTruthy();
