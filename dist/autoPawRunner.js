8a15566dfd0ef7cf850a0b8e5cda8829ce6054b1
Trent Grover <trent.grover@webfilings.com>
2014-09-29 00:19:34 -0500

    OSS license stuff

--- dist/autoPawRunner.js
+++ dist/autoPawRunner.js
@@ -3150,6 +3150,21 @@ jasmineRequire.QueryString = function() {
   return QueryString;
 };
 
+/*
+ * Copyright 2014 Workiva, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 function getJasmineRequireObj() {
   if (typeof module !== 'undefined' && module.exports) {
@@ -7350,6 +7365,22 @@ return Q;
 
     return Paw;
 }));
+
+/*
+ * Copyright 2014 Workiva, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 var autoPawRunner = (function () {   // jshint ignore:line
 
